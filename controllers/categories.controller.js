5b8bacb5006c015f5a3423d9e86f550d99e4cec1
Szpytma <szpytmapawel@hotmail.com>
2023-03-27 12:06:18 +0100

    3. GET/api/categories completed

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,7 +1,6 @@
 const { fetchAllCategories } = require("../models/categories.model");
 
 exports.getCategories = (req, res) => {
-  console.log("in controller");
   return fetchAllCategories().then((categories) => {
     res.status(200).send({ categories });
   });
--- models/categories.model.js
+++ models/categories.model.js
@@ -2,7 +2,6 @@ const db = require("../db/");
 const categories = require("../db/data/test-data/categories");
 
 exports.fetchAllCategories = () => {
-  console.log("in model");
   const selectAllCategories = "SELECT * FROM categories";
   return db.query(selectAllCategories).then((categories) => categories.rows);
 };
