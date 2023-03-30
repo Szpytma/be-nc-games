ebb6cbd572d0c19890fe371f915d809eab0358e1
Szpytma <szpytmapawel@hotmail.com>
2023-03-30 09:04:21 +0100

    task 8 completed

--- controllers/reviews.controller.js
+++ controllers/reviews.controller.js
@@ -1,4 +1,8 @@
-const { fetchReviewByID, fetchAllReviews } = require("../models/reviews.model");
+const {
+  fetchReviewByID,
+  fetchAllReviews,
+  updateReviewVotes,
+} = require("../models/reviews.model");
 
 exports.getReviewByID = (req, res, next) => {
   const id = req.params.review_id;
@@ -15,6 +19,19 @@ exports.getAllReviews = (req, res, next) => {
   fetchAllReviews()
     .then((reviews) => {
       res.status(200).send({ reviews });
+    })
+    .catch((error) => {
+      next(error);
+    });
+};
+
+exports.patchReviewVotes = (req, res, next) => {
+  const { review_id } = req.params;
+  const { body } = req;
+
+  updateReviewVotes(review_id, body)
+    .then((review) => {
+      res.status(200).send({ review });
     })
     .catch((error) => {
       next(error);
