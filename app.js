e58f662355931af4fbebec6d84aa7c85c3ec62d5
Szpytma <szpytmapawel@hotmail.com>
2023-03-31 15:53:56 +0100

    revert

--- app.js
+++ app.js
@@ -18,7 +18,14 @@ const endpoints = require("./endpoints.json");
 const app = express();
 app.use(express.json());
 
-app.get("/", getIndex);
+exports.getIndex = (req, res) => {
+  res.status(200).send(
+    `<h1>Please check <a href='https://github.com/Szpytma/nc-games#readme'> README</a> for instructions.</h1>
+      <br>
+      <h1>Please click <a href='api/'> this</a> for endpoints.</h1>`
+  );
+};
+
 app.get("/api", (req, res) => {
   res.send(endpoints);
 });
